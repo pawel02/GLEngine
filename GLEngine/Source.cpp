8e31fe7030193237046ed3d7cee5bc7771028050
pawel02 <pawel.bes13@gmail.com>
2021-01-14 15:28:34 +0000

    added better description of the project

--- GLEngine/Source.cpp
+++ GLEngine/Source.cpp
@@ -62,14 +62,14 @@ int main()
 
 
 	//vertecies
-	float vertecies[] = {
+	const float vertecies[] = {
 		 0.5f,  0.5f, 0.0f,     1.0f, 0.0f, 0.0f,      1.0f, 1.0f,// top right
 		 0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,      1.0f, 0.0f,// bottom right
 		-0.5f, -0.5f, 0.0f,     0.0f, 0.0f, 1.0f,      0.0f, 0.0f,// bottom left
 		-0.5f,  0.5f, 0.0f,     1.0f, 1.0f, 1.0f,      0.0f, 1.0f,// top left 
 	};
 
-	unsigned int indices[] = {
+	const unsigned int indices[] = {
 		0, 1, 3,   // first triangle
 		1, 2, 3    // second triangle
 	};
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # GLEngine
 I wanted to learn a little about graphics and game engines and therefore decided to learn opengl. I tried to keep the code as clean as possible however I am only begining my coding career in c++.
+I have used the following libraries:
+ - stbi_image (for loading images)
+ - glfw (cross platform for creating windows and handling keyboard input events)
+ - glad (cross platform loads all of the function pointers for opengl so)
